1787e466a618ef5dfe50aaede7dd3d7406a516f5
FloowDot <eli.chandlerr@gmail.com>
2023-03-25 05:22:31 +1300

    add app

---
+++ app.py
@@ -0,0 +1,35 @@
+from quart import Quart, request
+from proxyrotator import ProxyRotator
+import os
+
+app = Quart(__name__)
+
+db_uri = os.environ.get('DB_URI')
+proxy6_api_key = os.environ.get('PROXY6')
+
+p = ProxyRotator(proxy6_api_key, db_uri)
+
+@app.route('/', methods=['POST'])
+async def proxy_request():
+    payload = await request.get_json()
+    proxy_method = payload.get('proxy_method')
+    method = payload.get('method')
+    url = payload.get('url')
+    headers = payload.get('headers', None)
+    cookies = payload.get('cookies', None)
+    params = payload.get('params', None)
+    data = payload.get('data', None)
+    json_data = payload.get('json', None)
+
+    if proxy_method == 'static':
+        return await p.static(method, url, headers, cookies, params, data, json_data)
+    if proxy_method == 'rotate':
+        return await p.rotating(method, url, headers, cookies, params, data, json_data)
+
+@app.before_serving
+async def setup():
+    await p.setup()
+
+
+if __name__ == '__main__':
+    app.run()
